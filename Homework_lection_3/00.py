eee1d42930fd797d2546b55491262f7e4b652d52
Stefan Radev <stefanradev@Stefans-MacBook-Air.local>
2024-10-14 02:16:49 +0300

    homework 3

---
+++ Homework_lection_3/00.py
@@ -0,0 +1,6 @@
+number = int(input("Please enter a number to check: "))
+
+if number % 2 == 0:
+    print("even")
+else:
+    print("odd")
---
+++ Homework_lection_3/02.py
@@ -0,0 +1,7 @@
+while True:
+    user_choice = int(input("how much does 5 + 17 equal to: "))
+    if user_choice == 23:
+        print("Congrats")
+        break
+    else:
+        print("Try again")
